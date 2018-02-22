d6ba2aab1cd68f61c1ce3ac2832d25fd34791473
Leon K <leonknight@outlook.com>
2018-02-22 05:20:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,6 +23,9 @@ function whileLoop(n)
   return 'done'
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+
 function doWhileLoop(array)
 {
   do {
@@ -30,6 +33,3 @@ function doWhileLoop(array)
   } while (array.length > 0 && maybeTrue());
   return array
 }
-
-function maybeTrue() {
-  return Math.random() >= 0.5
