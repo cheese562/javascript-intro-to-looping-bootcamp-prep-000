f53b246faafeffc47ac88e52e82e40a7d7652212
Leon K <leonknight@outlook.com>
2018-02-22 05:07:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,12 +4,13 @@ function forLoop(array)
   {
     if (i === 1)
     {
-      [...array, 'I am 1 strange loop.']
+      return [...array, 'I am 1 strange loop.']
     }
     else 
     {
-      [...array, 'I am ${i} strange loops.']
+      return [...array, 'I am ${i} strange loops.']
     }  
   }
   return array
 }
+
